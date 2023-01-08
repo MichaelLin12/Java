6b23c46e3c3d7c15ab5b3557aeb879d1b337bee2
MichaelLin12 <michael.lin2032@gmail.com>
2023-01-07 22:18:29 -0600

    added junit testing

---
+++ SampleTest.java
@@ -0,0 +1,12 @@
+import java.beans.Transient;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+    
+    @Test
+    public void addNumbers(){
+        assertEquals(1+2, 3);
+    }
+}
