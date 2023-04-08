adb6de17736451aa9210dec829e0b20c7144a5e2
MichaelLin12 <michael.lin2032@gmail.com>
2023-04-08 17:19:57 -0500

    Random string generator

---
+++ StringGenerator.java
@@ -0,0 +1,27 @@
+public class StringGenerator {
+    public static void main(String[] args) {
+        // Generate a random string of length 100 using only lowercase letters
+        String[] records = generateRandomStrings(1000,100,26, 97);
+
+        // Print the generated string
+        for (int i = 0; i < records.length; i++) {
+            System.out.print(records[i]+"\n");
+        }
+    }
+
+    public static String[] generateRandomStrings(int size, int length, int range, int offset) {
+        String[] records = new String[size];
+        for (int i = 0; i < size; i++) {
+            records[i] = generateRandomString(length, range, offset);
+        }
+        return records;
+    }
+
+    public static String generateRandomString(int length, int range, int offset) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append((char) (Math.random() * range + offset));
+        }
+        return sb.toString();
+    }
+}
