d4e660eca0ff60520ec1010e19583e8089b99bac
MichaelLin12 <michael.lin2032@gmail.com>
2023-04-21 22:35:14 -0500

    understand binary heaps

--- MinHeap.java
+++ MinHeap.java
@@ -11,190 +11,125 @@ the key at the root must be minimum among all keys present in Binary Heap.
 The same property must be recursively true for all nodes in Binary Tree. 
 Max Binary Heap is similar to MinHeap. */
 
-import java.util.Arrays;
-
 public class MinHeap {
-	
-	private int length;
+
+	public MinHeap(int lenght) {
+		heap = new int [lenght];
+		size=0;
+	}
+	private int [] heap;
 	private int size;
-	private int[] heap;
-	
-	/* Constructor
-	 * 
+
+	/**
+	 * @param args
 	 */
-	public MinHeap() {
-		this.length = 10;
-		this.size = 0;
-		this.heap = new int[length];
-	}
-	
-	/* Prints the heap array
-	 * 
-	 */
-	public void printHeap() {
-		for (int i =0; i < this.heap.length; i++) {
-			System.out.println(this.heap[i]);
+	public static void main(String[] args) {
+		int [] input = {80, 50, 55, 76, 22, 90, 3}; 
+		MinHeap h = new  MinHeap(input.length);
+
+		for (int i : input) {
+			h.insert(i);
 		}
-	}
-	/* First checks if array is empty and returns and exception
-	 * if so.  Otherwise, returns the first element in the array
-	 */
-	public int peek() {
-		if (size == 0) {
-			throw new IllegalStateException();
-			
+
+		for (int i : h.heap) {
+			System.out.print(i+" ");
 		}
-		
-		return this.heap[0];
-	}
-	
-	/* Removes the smallest element from the heap
-	 * 
-	 */
-	public int pop() {
-		if (this.size == 0) {
-			throw new IllegalStateException();
+		System.out.println("");
+		for (int i = 0; i < h.heap.length; i++) {
+			System.out.print(h.remove()+" ");
 		}
-		
-		int element = this.heap[0];
-		this.size--;
-		heapifyDown();
-		return element;
-	}
-	
-	
-	/* Adds element to heap, maintains organization
-	 * 
-	 */
-	public void push(int element) {
-		expandCapacity();
-		this.heap[this.size] = element;
-		this.size++;
-		heapifyUp();
-	}
-	
-	
-	/* Bubbles smaller elements up to appropriate position in heap
-	 * 
-	 */
-	public void heapifyUp() {
-		int currIndex = this.size - 1;
-		while (hasParent(currIndex) && parent(currIndex) > this.heap[currIndex]) {
-			swap(getParentIndex(currIndex), currIndex);
-			currIndex = getParentIndex(currIndex);
-		}
-	}
-	
-	/* Bubbles larger elements down to appropriate position in heap
-	 * 
-	 */
-	public void heapifyDown() {
-		int currIndex = 0;
-		boolean notHeap = true;
-		while (hasLeftChild(currIndex) && notHeap) {
-			int smallerChildIndex = getLeftChildIndex(currIndex);
-			if (hasRightChild(currIndex) && rightChild(currIndex) < leftChild(currIndex)) {
-				smallerChildIndex = getRightChildIndex(currIndex);
-			}
-			
-			if (this.heap[currIndex] < this.heap[smallerChildIndex]) {
-				notHeap = false;
-			} else {
-				swap(currIndex, smallerChildIndex);
-			}
-			currIndex = smallerChildIndex;
-		}
-	}
-	
-	/* Swaps two elements in a given array
-	 * 
-	 */
-	private void swap(int indexOne, int indexTwo) {
-		int temp = this.heap[indexOne];
-		heap[indexOne] = heap[indexTwo];
-		heap[indexTwo] = temp;
-	}
-	
-	/* Checks if array is full, doubles capacity if so
-	 * 
-	 */
-	private void expandCapacity() {
-		if (this.size == this.length) {
-			this.heap = Arrays.copyOf(this.heap, this.length * 2);
-			this.length *= 2;
-			
-		}
-	}
-	
-	/* Returns corresponding index of left child in heap array
-	 * 
-	 */
-	private int getLeftChildIndex(int parentIndex) {
-	
-		return 2 * parentIndex + 1;
-	}
-	
-	/* Returns corresponding index of right child in heap array
-	 * 
-	 */
-	private int getRightChildIndex(int parentIndex) {
-		
-		return 2 * parentIndex + 2;
-	}
-	
-	/* Returns corresponding index of parent element in heap array
-	 * 
-	 */
-	private int getParentIndex(int childIndex) {
-		
-		return (childIndex - 1) / 2;
-	}
-	
-	/* Returns True if element at given index has a left child
-	 * 
-	 */
-	private boolean hasLeftChild(int index) {
-		
-		return getLeftChildIndex(index) < this.size;
-	}
-	
-	/* Returns True if element at given index has a right child
-	 * 
-	 */
-	private boolean hasRightChild(int index) {
-		
-		return getRightChildIndex(index) < this.size;
-	}
-	
-	/* Returns True if element at given index has a parent
-	 * 
-	 */
-	private boolean hasParent(int index) {
-		
-		return getParentIndex(index) >= 0;
-	}
-	
-	/* Returns value of the left child of the element at the given index
-	 * 
-	 */	
-	private int leftChild(int index) {
-		
-		return this.heap[getLeftChildIndex(index)];
+
 	}
 
-	/* Returns value of the right child of the element at the given index
-	 * 
-	 */	
-	private int rightChild(int index) {
-		
-		return this.heap[getRightChildIndex(index)];
+	/**
+	 * Return the left node 
+	 * @param position
+	 * @return
+	 */
+	private int left(int position) {
+		return position*2 +1;
 	}
-	
-	/* Returns value of the parent of the element at the given index
-	 * 
-	 */		
-	private int parent(int index) {
-		
-		return this.heap[getParentIndex(index)];
+
+	/**
+	 * Return the right node
+	 * @param position
+	 * @return
+	 */
+	private int right(int position) {
+		return position*2 +2;
 	}
+
+	/**
+	 * return the parent 
+	 * @param position
+	 * @return
+	 */
+	private int parent(int position) {
+		return position/2;
+	}
+
+	/**
+	 * leaf element check
+	 * @param position
+	 * @return
+	 */
+	private boolean isLeaf(int position) {
+		if(position >= (size / 2) && position <= size)
+			return true;
+		return false;
+	}
+
+	/**
+	 * Insert a element in heap.
+	 * @param i
+	 */
+	private  void insert(int i) {
+		if(size == 0) {
+			heap[0] = i;
+			size++;
+			return;
+		}
+
+		heap[size] = i;	
+		int current = size;
+		while(heap[current]<heap[parent(current)]) {
+			swap(current, parent(current));
+			current = parent(current);
+		}
+		size++;
+	}
+	/**
+	 * Remove element from min heap. remove the 0th position and replace it with last element .
+	 * @return
+	 */
+	private int remove() {
+		int val = heap[0];
+		heap[0]=heap[--size];
+		heapify(0);
+		return val;
+	}
+	private void swap(int i, int j) 
+	{ 
+		int tmp; 
+		tmp = heap[i]; 
+		heap[i] = heap[j]; 
+		heap[j] = tmp; 
+	} 
+
+	private void heapify(int position) {
+		if(isLeaf(position)){
+			return;
+		}
+
+		if(heap[position]>heap[left(position)]) {
+			swap(position,left(position));
+			heapify(left(position));
+		}
+
+		if(heap[position]>heap[right(position)]) {
+			swap(position,right(position));
+			heapify(right(position));
+		}
+	}
+
 }
